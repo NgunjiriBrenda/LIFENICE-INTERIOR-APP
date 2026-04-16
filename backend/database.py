cc9b743bac040b87e8040d18e647b914989f0ee2
brenda njoki <ngunjiribrenda5@gmail.com>
2026-04-16 23:10:24 +0300

    Creates database foundation

---
+++ backend/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import creative_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL =os.getenv("DATABASE_URL", "sqlite:///./lifeniceinteriors.db")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread":False}
+)
+
+SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
