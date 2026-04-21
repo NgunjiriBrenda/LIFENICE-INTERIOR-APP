3fe3766cad445f005e87b1737b1b850aded9032f
brenda njoki <ngunjiribrenda5@gmail.com>
2026-04-21 12:32:53 +0300

    Creates the models table

---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import (
+    Column, Integer, String, Float, Boolean, ForeignKey,DateTime, Text
+
+)
+
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+form database import Base
+import enum
+
