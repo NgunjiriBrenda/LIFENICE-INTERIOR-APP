7d1a9755972f8dc7c53677cad87fda1253093a96
brenda njoki <ngunjiribrenda5@gmail.com>
2026-04-22 16:36:12 +0300

    Creates the user logout

--- backend/schemas.py
+++ backend/schemas.py
@@ -19,3 +19,16 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
+
+class UserOut(BaseModel):
+    id: int
+    email: str
+    full_name: Optional[str]
+    phone_number: Optional[str]
+    is_admin: bool
+    is_active: bool
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
