82b2116e93332963962a220ec822549c105aced7
brenda njoki <ngunjiribrenda5@gmail.com>
2026-04-22 18:10:37 +0300

    Creates the mpesa data

--- backend/schemas.py
+++ backend/schemas.py
@@ -67,5 +67,100 @@ class ProductUpdate(BaseModel):
     is_active: Optional[bool] = None
 
 
+class ProductOut(BaseModel):
+    id: int
+    name: str
+    category: str
+    description: Optional[str]
+    price: float
+    image_url: Optional[str]
+    stock: int
+    is_active: bool
+    created_at: datetime
+
+class Config:
+    product_id: int
+    quantity: int = 1
+
+    @field_validator("quantity")
+    @classmethod
+    @classmethod
+    def quantity_must_be_positive(cls, v):
+        if v < 1:
+            raise ValueError("Quantity must be at least 1")
+
+        return v
+
+class CartItemUpdate(BaseModel):
+    quantity: int
+
+class CartItemOut(BaseModel):
+    id: int
+    product_id: int
+    quantity: int
+    product: ProductOut
+
+    class Config:
+        from_attributes = True
+
+class CartOut(BaseModel):
+    items: List[CartItemOut]
+    total: float
+    item_count: int
+
+
+class OrderItemOut(BaseModel):
+    id: int
+    product_id: Optional[int]
+    quantity: int
+    price: float
+    product: Optional[ProductOut]
+
+    class Config:
+        from_attributes = True
+
+class OrderCreate(BaseModel):
+    delivery_address: str
+    phone_number: str
+    notes: Optional[str] = None
+
+class OrderOut(BaseModel):
+    id: int
+    user_id: Optional[int]
+    total_amout: float
+    status: str
+    payment_reference: Optional[str]
+    delivery_address: Optional[str]
+    created_at: datetime
+    order_items: List[OrderItemOut]
+
+    class config:
+        from_attributes = True
+
+class OrderStatusUpdate(BaseModel):
+    status: str
+
+
+class MpesaSTKPushRequest(BaseModel):
+    order_id: int
+    phone_number: str
+
+class PaymentResponse(BaseModel):
+    success: bool
+    message: str
+    checkout_request_id: Optional[str] = None
+    order_id: Optional[int] = None
+
+
+
+
+
+
+
+
+
+
+
+
 
 
