0f3b88c1236b334369be4977dc8cc4c60b8893dd
brenda njoki <ngunjiribrenda5@gmail.com>
2026-04-23 17:28:35 +0300

    Creates router configurations

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -11,4 +11,13 @@ from database import get_db
 from model import User
 from schemas import UserRegister, UserLogin, UserOut, Token, TokenData
 
+load_dotenv()
+
+router = APIRouter(prefix="/auth", tags=["Aunthentication"])
+ 
+SECRET_KEY = os.getenv("SECRET_KEY", "lifeNIceinteriors-secret-key")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
