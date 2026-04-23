4a40d66a1122f0c35c8a6a3d3393e3a7a153e83b
brenda njoki <ngunjiribrenda5@gmail.com>
2026-04-23 17:56:57 +0300

    Creates tokens access

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -21,3 +21,15 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str ) -> bool:
+    return pwd_password_context.verify(plain_password, hashed_password)
+
+def create access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
