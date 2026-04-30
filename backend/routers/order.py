77e8769f6bc1a860495fef4b893d0c49df6b8f01
brenda njoki <ngunjiribrenda5@gmail.com>
2026-04-30 17:03:18 +0300

    Get all my orders uisng GET Crud Operations

--- backend/routers/order.py
+++ backend/routers/order.py
@@ -64,4 +64,16 @@ def create_order(
      db.refresh(order)
      return order
 
- 
+@router.get("/my-orders", response_model=List[OrderOut])
+def get_my_orders(
+     db: Session = Depends(get_db),
+     current_user: User = Depends(get_current_user)   
+):
+     return db.query(Order).filter(
+          Order.user_id == current_user.id
+
+     ).order_by(Order.created_at.desc()).all()
+     
+
+
+
