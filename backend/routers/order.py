b356e8300f9892b01e5258e42b0b1691f0fb8864
brenda njoki <ngunjiribrenda5@gmail.com>
2026-05-16 13:16:18 +0300

    Creates the CRUD operations for orders

--- backend/routers/order.py
+++ backend/routers/order.py
@@ -24,7 +24,7 @@ def create_order(
 
      if not cart_items:
           raise HTTPException(
-               status_code=400
+               status_code=400,
                detail = "Your cart is empty"
           )
      
@@ -73,7 +73,86 @@ def get_my_orders(
           Order.user_id == current_user.id
 
      ).order_by(Order.created_at.desc()).all()
+
+@router.get("/{order_id}", response_model=OrderOut)
+def get_order(
+     order_id: int,
+     db: Session = Depends(get_db),
+     current_user: User = Depends(get_current_user)
+):
+     order = db.query(Order).filter(
+          Order.id == order_id,
+          Order.user_id == current_user.id
+     ).first()
+     if not order:
+        raise HTTPException(status_code=404, detail="Order not found")
+     return order
+       
      
+
+@router.delete("/{order_id}/cancel")
+def cancel_order(
+     order_id: int,
+     db:Session = Depends(get_db),       
+     current_user: User = Depends(get_current_user)
+    
+):  
+     order = db.query(Order).filter(
+          Order.id == order_id,
+          Order.user_id == current_user.id
+
+    ).first()
+     if not order:
+       raise HTTPException(status_code=404, detail="Order not found")
+     if order.status != "pending":
+        raise HTTPException(status_code=400, detail="Only pending orders can be canceled")
+
+     for item in order.order_items:
+         if item.product:
+             item.product.stock += item.qunatity
+
+     order.status = "cancelled"
+     db.commit()
+
+     return {"message": "Order cancelled successfully"}
+
+
+@router.get("/admin/all", response_model=List[OrderOut])
+def admin_get_all_orders(
+     db: Session = Depends(get_db),
+     _: User = Depends(get_admin_user)
+
+):
+     return db.query(Order).order_by(Order.created_at.desc()).all()
+
+@router.patch("/admin/{order_id}/status", response_model=OrderOut)
+def admin_update_order_status(
+     order_id: int,
+     status_update: OrderStatusUpdate,
+     db: Session = Depends(get_db),
+     _: User = Depends(get_admin_user)
+ ):
+     valid_statuses = [
+          "pending", "paid", "processing",
+          "shipped", "deliver", "cancelled"
+     ]
+     if status_data.status not in valid_statuses:
+           raise HTTPException(
+               status_code=400,
+               detail=f"Invalid status.Choose from: {valid_statuses}"
+
+           )
+     order = db.query(Order).filter(Order.id == order_id).first()
+     if not order:
+          raise HTTPException(status_code=404, detail="Order not found")
+     
+     order.status = status_data.status
+     db.commit()
+     db.refresh(order)
+     return order
+
+
+
 
 
 
